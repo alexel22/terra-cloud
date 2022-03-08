843b60871a3bb3053f7029070dd142c7f06b7722
alexel22 <44574421+alexel22@users.noreply.github.com>
2022-03-08 12:41:17 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,8 @@ terraform {
   required_providers {
     azurerm = {
       source = "hashicorp/azurerm"
-      version = "2.98.0"
+      # version = "2.98.0"
+      version = "<=2.0.0"
     }
   }
 }
