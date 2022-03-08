5ac17fe039fdcfcd524defdac20dd8027f6e361b
alexel22 <44574421+alexel22@users.noreply.github.com>
2022-03-08 11:29:28 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,16 +9,16 @@ terraform {
 
 provider "azurerm" {
     features {
-    # version       = ">0.2"
-    # subscription  = "{var.subscription_id}"
-    # client_id     = "${var.client_secret}"
-    # cliente_secret= "${var.client_secret}"
-    # tenant_id     = "${var.tenant_id}"
+      version       = "<=2.0.0"
+      subscription  = var.subscriptionID
+      client_id     = var.clientID
+      cliente_secret= var.clientSecret
+      tenant_id     = var.tenantID
     }
 }
 
 # Create a resource group
-resource "azurerm_resource_group" "example" {
+resource "azurerm_resource_group" "tenaris-rg" {
   name     = var.rg
   location = var.location
     tags = {
